3103f977f6aa44d705cdf5af72aca0d0b77199e9
dillanfdr <nurfadillahdwirahma@gmail.com>
2024-11-06 21:49:06 +0700

    update inspiration full

--- javascript/inspiration1.js
+++ javascript/inspiration1.js
@@ -36,3 +36,20 @@ const slider = document.querySelector('.frameParent');
 let isDown = false;
 let startX;
 let scrollLeft;
+
+slider.addEventListener('mousedown', () => {
+isDown = true;
+});
+
+slider.addEventListener('mouseleave', () => {
+isDown = false;
+});
+
+slider.addEventListener('mouseup', () => {
+isDown = false
+});
+
+slider.addEventListener('mousemove', () => {
+console.log(isDown);
+console.log('Do work!');
+});
