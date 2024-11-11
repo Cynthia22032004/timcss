e2b79fdedeb13f9798d9dfb1b6448e257f56f74f
Cynthia22032004 <cynthiadwi21222@gmail.com>
2024-11-11 22:29:14 +0700

    test

---
+++ javascript/article.js
@@ -0,0 +1,104 @@
+var yellowAndGreenModernLogo = document.getElementById('yellowAndGreenModernLogo');
+if (yellowAndGreenModernLogo) {
+  yellowAndGreenModernLogo.addEventListener('click', function (e) {
+    // Add your code here
+  });
+}
+
+var frameImage = document.getElementById('frameImage');
+if (frameImage) {
+  frameImage.addEventListener('click', function () {
+    window.open('https://www.fifgroup.co.id/danastra/eco-house-rumah-ramah-lingkungan-dan-hemat-biaya');
+  });
+}
+
+var textContainer = document.getElementById('textContainer');
+if (textContainer) {
+  textContainer.addEventListener('click', function () {
+    window.open('https://www.fifgroup.co.id/danastra/eco-house-rumah-ramah-lingkungan-dan-hemat-biaya');
+  });
+}
+
+var frameImage1 = document.getElementById('frameImage1');
+if (frameImage1) {
+  frameImage1.addEventListener('click', function () {
+    window.open('https://blog.mediasarana.com/__trashed-5/');
+  });
+}
+
+var textContainer1 = document.getElementById('textContainer1');
+if (textContainer1) {
+  textContainer1.addEventListener('click', function () {
+    window.open('https://blog.mediasarana.com/__trashed-5/');
+  });
+}
+
+var frameImage2 = document.getElementById('frameImage2');
+if (frameImage2) {
+  frameImage2.addEventListener('click', function () {
+    window.open('https://www.solarkita.com/blog/apa-itu-eco-house-design-ini-pengertian-hingga-manfaatnya');
+  });
+}
+
+var textContainer2 = document.getElementById('textContainer2');
+if (textContainer2) {
+  textContainer2.addEventListener('click', function () {
+    window.open('https://www.solarkita.com/blog/apa-itu-eco-house-design-ini-pengertian-hingga-manfaatnya');
+  });
+}
+
+var imgImage = document.getElementById('imgImage');
+if (imgImage) {
+  imgImage.addEventListener('click', function () {
+    window.open('https://artigianfer.com/en/news/greenhouses-how-to-reduce-pallution-and-waste/');
+  });
+}
+
+var bodyContainer = document.getElementById('bodyContainer');
+if (bodyContainer) {
+  bodyContainer.addEventListener('click', function () {
+    window.open('https://artigianfer.com/en/news/greenhouses-how-to-reduce-pallution-and-waste/');
+  });
+}
+
+var bodyContainer1 = document.getElementById('bodyContainer1');
+if (bodyContainer1) {
+  bodyContainer1.addEventListener('click', function () {
+    window.open('https://upland.psp.pertanian.go.id/public/artikel/1672411257/sistem-smart-green-house-sebagai-pendongkrak-produktivitas-tanaman');
+  });
+}
+
+var imgImage1 = document.getElementById('imgImage1');
+if (imgImage1) {
+  imgImage1.addEventListener('click', function () {
+    window.open('https://upland.psp.pertanian.go.id/public/artikel/1672411257/sistem-smart-green-house-sebagai-pendongkrak-produktivitas-tanaman');
+  });
+}
+
+var imgImage2 = document.getElementById('imgImage2');
+if (imgImage2) {
+  imgImage2.addEventListener('click', function () {
+    window.open('https://myeco.id/hemat-energi-eco-green-living-konsep-tepat-membuat-rumah-modern/');
+  });
+}
+
+var bodyContainer2 = document.getElementById('bodyContainer2');
+if (bodyContainer2) {
+  bodyContainer2.addEventListener('click', function () {
+    window.open('https://myeco.id/hemat-energi-eco-green-living-konsep-tepat-membuat-rumah-modern/');
+  });
+}
+
+var frameContainer1 = document.getElementById('frameContainer1');
+if (frameContainer1) {
+  frameContainer1.addEventListener('click', function (e) {
+    // Add your code here
+  });
+}
+
+var newsContainer = document.getElementById('newsContainer');
+if (newsContainer) {
+  newsContainer.addEventListener('click', function () {
+    window.open('https://mortgagemaster.co.id/blog/apa-itu-rumah-eco-green-simak-penjelasan-lengkapnya-ini-190');
+  });
+}
